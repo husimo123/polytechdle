acff0fca954f13ed2727e3a90dc9edd6e8135efa
tnavet <titouan.navet@etud.univ-angers.fr>
2025-04-01 11:36:26 +0200

    ajout du professeur fixe dans classique mais il reste des problèmes+ ajout de l'anglais sur le footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useLanguage } from '../LanguageContext';
 
 function Footer() {
+  const { language } = useLanguage();
+
+  const texts = {
+    fr: {
+      copyright: "&copy; 2025 SIMON Hugo & NAVET Titouan & HODE Dorian. Tous droits réservés.",
+    },
+    en: {
+      copyright: "&copy; 2025 SIMON Hugo & NAVET Titouan & HODE Dorian. All rights reserved.",
+    },
+  };
+
   return (
     <footer>
-      <p>&copy; 2025 SIMON Hugo & NAVET Titouan & HODE Dorian. Tous droits réservés.</p>
+      <p dangerouslySetInnerHTML={{ __html: texts[language].copyright }}></p>
     </footer>
   );
 }
